97f1ac47d54be8f8a36680725c7521b9475a6ce0
Denis <jaromirabramov@gmail.com>
2023-01-19 22:40:08 +0600

    init

---
+++ src/test/java/mira/space/restfulwebservice/RestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mira.space.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
